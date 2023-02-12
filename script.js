636ae0e44bf2461bc0b3e8740cf3781d56a3d2bc
andreabeggio <beggio.andrea@gmail.com>
2023-02-12 23:04:37 +0100

    added dino

--- script.js
+++ script.js
@@ -17,98 +17,35 @@ rectangle = {
 
 };
 
-controller = {
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
 
-  left:false,
-  right:false,
-  up:false,
-  keyListener:function(event) {
+function jump() {
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
 
-    var key_state = (event.type == "keydown")?true:false;
-
-    switch(event.keyCode) {
-
-      case 37:// left key
-        controller.left = key_state;
-      break;
-      case 38:// up key
-        controller.up = key_state;
-      break;
-      case 39:// right key
-        controller.right = key_state;
-      break;
-
-    }
-
+    setTimeout(function () {
+      dino.classList.remove("jump");
+    }, 300);
   }
+}
 
-};
+let isAlive = setInterval(function () {
+  // get current dino Y position
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
 
-loop = function() {
+  // get current cactus X position
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left")
+  );
 
-  if (controller.up && rectangle.jumping == false) {
-
-    rectangle.y_velocity -= 20;
-    rectangle.jumping = true;
-
+  // detect collision
+  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+    // collision
+    alert("Game Over!");
   }
+}, 10);
 
-  if (controller.left) {
-
-    rectangle.x_velocity -= 0.5;
-
-  }
-
-  if (controller.right) {
-
-    rectangle.x_velocity += 0.5;
-
-  }
-
-  rectangle.y_velocity += 1.5;// gravity
-  rectangle.x += rectangle.x_velocity;
-  rectangle.y += rectangle.y_velocity;
-  rectangle.x_velocity *= 0.9;// friction
-  rectangle.y_velocity *= 0.9;// friction
-
-  // if rectangle is falling below floor line
-  if (rectangle.y > 180 - 16 - 32) {
-
-    rectangle.jumping = false;
-    rectangle.y = 180 - 16 - 32;
-    rectangle.y_velocity = 0;
-
-  }
-
-  // if rectangle is going off the left of the screen
-  if (rectangle.x < -32) {
-
-    rectangle.x = 320;
-
-  } else if (rectangle.x > 320) {// if rectangle goes past right boundary
-
-    rectangle.x = -32;
-
-  }
-
-  context.fillStyle = "#202020";
-  context.fillRect(0, 0, 320, 180);// x, y, width, height
-  context.fillStyle = "#ff0000";// hex for red
-  context.beginPath();
-  context.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
-  context.fill();
-  context.strokeStyle = "#202830";
-  context.lineWidth = 4;
-  context.beginPath();
-  context.moveTo(0, 164);
-  context.lineTo(320, 164);
-  context.stroke();
-
-  // call update when the browser is ready to draw again
-  window.requestAnimationFrame(loop);
-
-};
-
-window.addEventListener("keydown", controller.keyListener)
-window.addEventListener("keyup", controller.keyListener);
-window.requestAnimationFrame(loop);
+document.addEventListener("keydown", function (event) {
+  jump();
+});
